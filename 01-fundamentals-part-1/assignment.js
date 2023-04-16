53c4c835418e01d868070e75731c022008dd33bc
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-04-16 17:35:41 +0200

    Values and variables

---
+++ 01-fundamentals-part-1/assignment.js
@@ -0,0 +1,6 @@
+// LEZIONE 1: Values and Variables (Sez. 2, lez. 10)
+let country = "Italia";
+let continent = "Europa";
+let population = 59.11;
+
+console.log(country, continent, population);
---
+++ 01-fundamentals-part-1/script.js
@@ -0,0 +1,29 @@
+// LEZIONE 1: Values and Variables (Sez. 2, lez. 10)
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda"; // camelCase con 2 parole
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let firstNamePerson; // camelCase con più di 2 parole
+let first_name_person; // popolare in linguaggi come Ruby
+
+// I nomi delle variabili possono contenere solo NUMERI (NON come primo elemento), LETTERE, UNDERSCORES e $
+
+// Alcune parole sono keyword RISERVATE, per cui non si possono utilizzare per nominare le variabili (esempio: function, new, ecc.)
+let $function; // Metodi per ovviare alle keywords
+let _new; // Metodi per ovviare alle keywords
+
+let person = "jonas";
+let PI = 3.1415; // Pi-greco, la variabile in MAIUSCOLO indica, in realtà, una costante
+
+let myFirstJob = "Programmer"; // NO: let job1 = 'programmer';
+let myCurrentJob = "Teacher"; // NO: let job2 = 'teacher';
+
+console.log(myFirstJob, myCurrentJob);
