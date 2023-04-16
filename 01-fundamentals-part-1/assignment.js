8cc437db017bc40f9f6fc77f83fa9cb3fba8f6cf
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-04-16 17:40:28 +0200

    Data types

--- 01-fundamentals-part-1/assignment.js
+++ 01-fundamentals-part-1/assignment.js
@@ -1,6 +1,20 @@
+/*
 // LEZIONE 1: Values and Variables (Sez. 2, lez. 10)
 let country = "Italia";
 let continent = "Europa";
 let population = 59.11;
 
 console.log(country, continent, population);
+*/
+
+///////////////////////////////////////////////
+// LEZIONE 2: Data Types (Sez. 2, lez. 12)
+let isIsland = false;
+let language;
+
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -1,3 +1,4 @@
+/*
 // LEZIONE 1: Values and Variables (Sez. 2, lez. 10)
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
@@ -27,3 +28,29 @@ let myFirstJob = "Programmer"; // NO: let job1 = 'programmer';
 let myCurrentJob = "Teacher"; // NO: let job2 = 'teacher';
 
 console.log(myFirstJob, myCurrentJob);
+*/
+
+///////////////////////////////////////////////
+// LEZIONE 2: Data Types (Sez. 2, lez. 12)
+true; // valore BOOLEANO
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// typeof è un OPERATORE SPECIALE che permette di capire il tipo di dato di un valore
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+// JS consente la TIPIZZAZIONE DINAMICA, ossia rende possibile assegnare un altro tipo di valore ad una variabile già dichiarata
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun, javascriptIsFun);
+
+let year;
+console.log(typeof year, year);
+
+year = 1991;
+console.log(typeof year, year);
+
+// CURIOSITà BUG: il valore null per JS viene tipizzato come un object, invece di essere definito undefined, questo è chiaramente un BUG che però non viene corretto per motivi di legacy (eredità)
+console.log(typeof null);
