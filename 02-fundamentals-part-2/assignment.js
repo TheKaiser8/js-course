2ad6941c729457ffd3814ef2a7db953832e72546
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-04-27 23:47:56 +0200

    Functions and few examples

--- 02-fundamentals-part-2/assignment.js
+++ 02-fundamentals-part-2/assignment.js
@@ -1,1 +1,19 @@
 "use strict";
+
+//////////////////////////////////
+// LEZIONE 2: Functions (Sez. 3, lez. 33)
+function describeCountry(country, population, capitalCity) {
+  // const yourCountry = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+  // return yourCountry; // forma estesa salvando la stringa in una variabile
+  return `${country} has ${population} million people and its capital city is ${capitalCity}.`; // forma contratta
+}
+
+const italy = describeCountry("Italy", 59, "Rome");
+console.log(italy);
+
+const germany = describeCountry("Germany", 83, "Berlin");
+console.log(germany);
+
+const finland = describeCountry("Finland", 6, "Helsinki");
+console.log(finland);
+// non fatto un console.log unico per non avere una stringa unica
--- 02-fundamentals-part-2/script.js
+++ 02-fundamentals-part-2/script.js
@@ -1,8 +1,10 @@
+//////////////////////////////////
 // LEZIONE 1: Activating Strict Mode (Sez. 3, lez. 32)
 
 "use strict"; // per attivare la modalità rigorosa di questo script JS
 // con questa modalità rendiamo il codice JS più sicuro perché evitiamo possibili errori accidentali e rendiamo visibili gli errori
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -12,3 +14,39 @@ if (hasDriversLicense) console.log("I can drive");
 // la modalità STRICT MODE ci aiuta anche a non utilizzare come nome di variabili parole riservate:
 // const interface = "Audio";
 // const private = 534;
+*/
+
+//////////////////////////////////
+// LEZIONE 2: Functions (Sez. 3, lez. 33)
+// Function: pezzo di codice che possiamo riutilizzare più volte e che può ricevere o restituire dati
+
+// questa è una semplice funzione didattica che stampa in console una stringa, non restituisce alcun valore
+function logger() {
+  console.log("My name is Jonas");
+}
+
+// calling / running / invoking function (sono parole diverse per indicare l'utilizzo di una funzione)
+logger();
+logger();
+logger();
+
+// funzione con 2 parametri (il parametro è il segnaposto, quindi in questo caso 'apples' e 'oranges')
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice; // restituisce la variabile prodotta con questa funzione
+}
+
+// fruitProcessor(5, 0); // 5 e 0 sono gli argomenti della funzione, quindi il valore dei parametri
+// per visualizzare il valore di questa funzione dobbiamo memorizzarla in una variabile
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+// Avremmo potuto anche stamparlo direttamente in console senza memorizzarlo in una variabile:
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+// Number: funzione che converte una stringa in un numero
+const num = Number("23");
+console.log(num);
