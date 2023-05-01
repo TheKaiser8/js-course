4e36d0302ae031a45b0e9a97162607e2ec3ecfe4
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-05-02 01:56:32 +0200

    The Spread Operator (...)

--- 08-data-structures-operators/script.js
+++ 08-data-structures-operators/script.js
@@ -44,6 +44,13 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+
+  // aggiungo metodo per ordinare pasta con esattamente 3 ingredienti:
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
 
 /*
@@ -104,6 +111,7 @@ const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r); // output 8 9 1
 */
 
+/*
 ////////////////////////////////////
 // LEZIONE 2: Destructuring Objects (Sez. 9, Lez. 104)
 const { name, openingHours, categories } = restaurant; // utilizziamo i nomi esatti delle proprietà dell'oggetto (l'ordine non è importante perché non è un'array)
@@ -154,3 +162,71 @@ restaurant.orderDelivery({
 restaurant.orderDelivery({
   address: 'Via del Sole, 21',
 });
+*/
+
+////////////////////////////////////
+// LEZIONE 3: The Spread Operator (...) (Sez. 9, Lez. 105)
+
+// Esempio ESPANSIONE di un ARRAY:
+const arr = [7, 8, 9];
+// con ciclo for aggiungeremmo cosi 2 valori all'inizio dell'array creando un nuovo array
+const newArrayLoop = [1, 2];
+for (let i = 0; i < arr.length; i++) {
+  const element = arr[i];
+  newArrayLoop.push(element);
+}
+console.log(newArrayLoop); // [ 1, 2, 7, 8, 9 ]
+
+// con SPREAD OPERATOR:
+const newArray = [1, 2, ...arr];
+console.log(newArray); // [ 1, 2, 7, 8, 9 ]
+
+// inoltre, posso utilizzarlo per registrare i singoli elementi
+console.log(...newArray); // 1, 2, 7, 8, 9
+
+// Esempio per espandere il mainMenu dell'oggetto restaurant
+const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
+console.log(newMenu); // [ "Pizza", "Pasta", "Risotto", "Gnocchi" ]
+console.log(...newMenu); // Pizza Pasta Risotto Gnocchi
+
+// *** N.B. La grande differenza tra la DESTRUTTURAZIONE e lo SPREAD OPERATOR è che quest'ultimo prende TUTTI gli elementi dell'array e NON crea nuove variabili, pertanto possiamo usarlo solo dove abbiamo bisogno di scrivere valori separati da virgole
+
+// Copiare un array (SHALLOW COPY):
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy); // [ "Pizza", "Pasta", "Risotto" ]
+mainMenuCopy.push('Pane');
+console.log(mainMenuCopy); // [ "Pizza", "Pasta", "Risotto", "Pane" ]
+console.log(restaurant.mainMenu); // [ "Pizza", "Pasta", "Risotto" ] non modifico l'array originale contenuto nell'oggetto
+
+// Unire 2 array (Join 2 arrays):
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu); // [ "Pizza", "Pasta", "Risotto", "Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad" ]
+
+// *** N.B. Lo SPREAD OPERATOR funziona su tutti gli ITERABILI, ossia cose come ARRAY, STRINGS, MAPS O SETS, ma NON sugli OGGETTI
+const str = 'Jonas';
+const letters = [...str, ' ', 'S.'];
+console.log(letters); // [ "J", "o", "n", "a", "s", " ", "S." ]
+console.log(...str); // J o n a s
+
+// NON possiamo, però, creare una stringa utilizzando il template literal:
+// console.log(`${...str} Schmedtmann`); // NON funziona perché è un posto in cui non si aspetta valori separati da virgola
+
+// richiamo metodo creato a riga 49, qui vado a specificare i 3 parametri (3 ingredienti)
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt("Let's make pasta! Ingredient 2?"),
+//   prompt("Let's make pasta! Ingredient 3?"),
+// ];
+// console.log(ingredients);
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]); // bad solution
+// restaurant.orderPasta(...ingredients); // better solution with spread operator
+
+// da ES2018 lo SPREAD OPERATOR funziona anche sugli oggetti che non sono ITERABILI
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Giuseppe' };
+console.log(newRestaurant);
+
+// Copiare un oggetto (SHALLOW COPY):
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name); // Ristorante Roma
+console.log(restaurant.name); // Classico Italiano
