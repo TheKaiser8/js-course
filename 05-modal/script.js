8784d1ad569bfcd2c6bafea48233cfed77047e16
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-05-01 19:08:01 +0200

    Working With Classes

--- 05-modal/script.js
+++ 05-modal/script.js
@@ -13,3 +13,32 @@ console.log(btnsOpenModal);
 for (let i = 0; i < btnsOpenModal.length; i++)
   console.log(btnsOpenModal[i].textContent); // visualizzo in console il testo di ogni button che mostra la modale
 // con una istruzione su una sola riga posso evitare di utilizzare le parentesi graffe per aprire e chiudere il ciclo for
+
+/////////////////////////////////////
+// LEZIONE 2: Working With Classes (Sez. 7, lez. 80)
+
+// creo variabile come closeModal anche per l'apertura della modale
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// posso paragonare NodeList ad un array, quindi posso fare un ciclo su di esso
+for (let i = 0; i < btnsOpenModal.length; i++)
+  /*
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden'); // NON si utilizza il punto (.hidden), il punto è solo per il selettore
+    overlay.classList.remove('hidden'); // rimuovendo la classe hidden posso visualizzare l'overlay con la modale aperta
+  }); // visualizzo in console il testo di ogni button che mostra la modale
+*/
+  btnsOpenModal[i].addEventListener('click', openModal);
+// con una istruzione su una sola riga posso evitare di utilizzare le parentesi graffe per aprire e chiudere il ciclo for
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal); // NON richiamiamo la funzione closeModal(), ma sarà JavaScript a richiamarla solo nel momento in cui si verificherà l'evento click. Noi immettiamo solamento la variabile closeModal come argomento della funzione
+overlay.addEventListener('click', closeModal);
