f7604fc49f8f5814e68c6fd43eb5c0259e1132c4
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-05-02 01:51:37 +0200

    Destructuring Objects

--- 08-data-structures-operators/script.js
+++ 08-data-structures-operators/script.js
@@ -27,8 +27,26 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]; // restituisco array
   },
+
+  // aggiungo metodo per la consegna dell'ordine (come argomento della funzione passiamo direttamente un intero oggetto):
+  // orderDelivery: function (obj) {
+  //   console.log(obj);
+  // },
+  // possiamo eseguire subito la destrutturazione negli argomenti della funzione
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(starterIndex, mainIndex, time, address);
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
+/*
 ////////////////////////////////////
 // LEZIONE 1: Destructuring Arrays (Sez. 9, Lez. 103)
 // DESTRUCTURING: funzionalità ES6, è un modo per decomprimere i valori, da un ARRAY o un OGGETTO, in variabili separate. Significa suddividere una struttura dati complessa in una struttura dati più piccola come una variabile.
@@ -84,3 +102,55 @@ console.log(i, j, k); // output 2 5 6
 // possiamo definire un valore di default se non presente:
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r); // output 8 9 1
+*/
+
+////////////////////////////////////
+// LEZIONE 2: Destructuring Objects (Sez. 9, Lez. 104)
+const { name, openingHours, categories } = restaurant; // utilizziamo i nomi esatti delle proprietà dell'oggetto (l'ordine non è importante perché non è un'array)
+console.log(name, openingHours, categories);
+
+// per creare variabili con nome diverso dalle proprietà dell'oggetto:
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// per creare valori di default alle proprietà (array vuoto in questo caso):
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutazioni variabili:
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+// JS si aspetta sempre un blocco se una riga di codice inizia con le parentesi graffe, quindi non possiamo assegnare nulla ad un blocco di codice. Per aggirare questo problema possiamo racchiudere tutto tra parentesi tonde:
+({ a, b } = obj); // in questo modo possiamo riassegnare i valori delle variabili utilizzando le proprietò dell'oggetto
+console.log(a, b);
+
+// Destructuring oggetti annidati (nested object):
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log(open, close);
+
+// posso anche riassegnare il nome di queste proprietà dell'oggetto annidato
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+// richiamo metodo creato a riga 36, qui vado a specificare il parametro (che è l'oggetto indicato come argomento nel metodo creato)
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+// con valori di default specificati con il destructuring nell'argomento della funzione
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+});
