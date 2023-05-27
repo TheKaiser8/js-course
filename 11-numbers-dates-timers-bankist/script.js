3875b909e2cb4c47258c18830cc1c74fb07b17e3
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-05-27 18:09:51 +0200

    Math and Rounding - Math: Math.sqrt, Math.max, Math.min, Math.random - Rounding: Math.trunc, Math.round, Math.ceil, Math.floor, toFixed

--- 11-numbers-dates-timers-bankist/script.js
+++ 11-numbers-dates-timers-bankist/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -182,7 +182,9 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
+
   const amount = Number(inputTransferAmount.value);
+
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +208,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -252,6 +254,7 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+/*
 //////////////////////////////////
 // LEZIONE 1: Converting and Checking Numbers (Sez. 12, Lez. 170)
 
@@ -301,3 +304,63 @@ console.log(Number.isNaN(23 / 0)); // false
 console.log(Number.isInteger(23)); // true
 console.log(Number.isInteger(23.0)); // true
 console.log(Number.isInteger(23 / 0)); // false
+*/
+
+//////////////////////////////////
+// LEZIONE 2: Math and Rounding (Sez. 12, Lez. 171)
+
+// RADICE QUADRATA:
+console.log(Math.sqrt(25)); // 5
+// oppure
+console.log(25 ** (1 / 2)); // 5
+
+// RADICE CUBICA:
+console.log(8 ** (1 / 3)); // 2
+
+// VALORE MAX: restituisce il valore max dei numeri passati
+console.log(Math.max(5, 18, 23, 11, 2)); // 23
+console.log(Math.max(5, 18, '23', 11, 2)); // 23, perché esegue TYPE COERCION
+console.log(Math.max(5, 18, '23px', 11, 2)); // NaN, perché NON esegue il PARSING
+
+// VALORE MIN: restituisce il valore min dei numeri passati
+console.log(Math.min(5, 18, 23, 11, 2)); // 2
+
+// CALCOLO AREA di un CERCHIO con RAGGIO 10px (PI: costante pi-greco):
+console.log(Math.PI * Number.parseFloat('10px') ** 2); // 314.1592653589793
+
+// GENERARE NUMERO CASUALE:
+console.log(Math.trunc(Math.random() * 6) + 1); // numero intero da 1 a 6
+
+// ARROTONDAMENTO (ROUNDING) a NUMERO INTERO: Math.trunc
+console.log(Math.trunc(23.3)); // 23
+console.log(Math.trunc(23.9)); // 23
+// ARROTONDAMENTO (ROUNDING) a NUMERO INTERO più VICINO: Math.round
+console.log(Math.round(23.3)); // 23
+console.log(Math.round(23.9)); // 24
+// ARROTONDAMENTO (ROUNDING) a NUMERO INTERO per ECCESSO: Math.ceil
+console.log(Math.ceil(23.3)); // 24
+console.log(Math.ceil(23.9)); // 24
+// ARROTONDAMENTO (ROUNDING) a NUMERO INTERO per DIFETTO: Math.floor
+console.log(Math.floor(23.3)); // 23
+console.log(Math.floor(23.9)); // 23
+
+// *** Tutti questi metodi fanno anche la TYPE COERCION
+console.log(Math.trunc('23.9')); // 23
+console.log(Math.round('23.9')); // 24
+console.log(Math.ceil('23.9')); // 24
+console.log(Math.floor('23.9')); // 23
+
+// *** N.B. Math.trunc e Math.floor eseguono sostanzialmente lo stesso calcolo se si tratta di NUMERI POSITIVI, ma NON quando si tratta di NUMERI NEGATIVI
+console.log(Math.trunc(-23.3)); // -23
+console.log(Math.floor(-23.3)); // -24 Math.floor funziona in modo migliore con i NUMERI NEGATIVI
+
+// FUNZIONE per GENERARE NUMERO CASUALE:
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+console.log(randomInt(1, 6)); // numero intero da 1 a 6
+
+// ARROTONDAMENTO (ROUNDING) NUMERI DECIMALI con numero che diventa STRINGA: toFixed()
+console.log((2.7).toFixed(0)); // '3'
+console.log((2.7).toFixed(3)); // '2,700'
+console.log((2.345).toFixed(2)); // '2,35'
+console.log(+(2.345).toFixed(2)); // 2,35 --> OPERATORE + per convertire STRINGA in NUMERO
